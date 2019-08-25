3f5dbbe2fdf976d1ee442a9b6a9f29a064d80e3d
Youli Tuo <crazuni1993@gmail.com>
2019-08-25 22:55:23 +0800

    version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hxmt_analysis_demo",
-    version="0.0.3",
+    version="0.1.0",
     author="Youli Tuo",
     author_email="tuoyl@ihep.ac.cn",
     description="a package for hxmt analysis",
